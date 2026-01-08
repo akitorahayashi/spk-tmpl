7b16ed0aeadb55942a9f7ffa74ab68bc40af6df5
akitorahayashi <hayashi20030703@gmail.com>
2026-01-08 20:33:00 +0900

    refactor: Simplify SKSpriteNode and SKTexture initializers for ImageAsset

--- Packages/Packages/SharedResources/Sources/SharedResources/SKSpriteNode+Asset.swift
+++ Packages/Packages/SharedResources/Sources/SharedResources/SKSpriteNode+Asset.swift
@@ -1,22 +1,17 @@
 #if canImport(SpriteKit) && canImport(UIKit)
-  import SpriteKit
-  import UIKit
+import SpriteKit
+import UIKit
 
-  public extension SKSpriteNode {
-    /// Creates a sprite node with texture from a type-safe image asset.
-    ///
-    /// Uses `imageNamed` internally to leverage SpriteKit's texture caching.
+public extension SKSpriteNode {
     convenience init(asset: ImageAsset) {
-      self.init(imageNamed: asset.name)
+        let texture = SKTexture(image: asset.image)
+        self.init(texture: texture, color: .clear, size: texture.size())
     }
-  }
+}
 
-  public extension SKTexture {
-    /// Creates a texture from a type-safe image asset.
-    ///
-    /// Uses `imageNamed` internally to leverage SpriteKit's texture caching.
+public extension SKTexture {
     convenience init(asset: ImageAsset) {
-      self.init(imageNamed: asset.name)
+        self.init(image: asset.image)
     }
-  }
+}
 #endif
